a8ea02bc5b9118bf1134adee8a3e103ea097a838
Muslima <muslima@gmail.com>
2024-07-02 23:45:31 +0500

    added files

---
+++ managment.py
@@ -0,0 +1,21 @@
+import psycopg2 as psql
+class database:
+    @staticmethod
+    def connect(query:str, query_type:str):
+        db = psql.connect(
+            database = 'jam',
+            user = 'postgres',
+            password = 'muslima2004',
+            host = 'localhost',
+            port = "5432"
+        )
+        cursor = db.cursor()
+        cursor.execute(query)
+        data = ['create', 'delete', 'insert', 'update']
+        if query_type in data:
+            db.commit()
+            return f"""{query_type} query successfully"""
+        else:
+            return cursor.fetchall()
+
+
